36aca727f9737a51805983edcd53ed9b1198624f
joaquim-almeida <joaquimrafael30@hotmail.com>
2021-10-07 21:40:04 -0300

    adicionado rota de delete

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -17,8 +17,50 @@ router.post("/categories/save", (req, res) => {
             res.redirect("/");
         });
     }else{
-        res.redirect("/admin/categories/new");
+        res.redirect("admin/categories/new");
     }
-})
+});
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then((categories) =>{
+        res.render("admin/categories/index", {categories: categories});
+    });
+});
+
+router.get("/categores/delete/:id", (req, res) =>{
+    let id = req.params.id;
+
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.render("admin/categorias/index");
+            });
+        }else{
+            res.render("admin/categorias/index");
+        }
+    }else{
+        res.render("admin/categorias/index");
+    }
+});
+
+//como diria o pedreiro em construção
+router.get("admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id;
+    Category.findAll({
+        where: {
+            id: id
+        }
+    }).then((category) => {
+        res.render("", {category: category});
+    });
+});
+
+router.post("admin/categories/edit", (req, res) => {
+    
+});
 
 module.exports = router;
